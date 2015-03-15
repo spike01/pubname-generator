42af1bc1bcaa35464938866d241bedf147902f23
Spike Nox <spike01@gmail.com>
2015-03-15 17:02:30 +0000

    Adds grunt-jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,19 @@ module.exports = function(grunt){
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'src/**/*.js', 'public/javascripts/src/*.js', 'spec/**/*.js']
+    },
+    jasmine: {
+      src: 'public/javascripts/src/*.js',
+      options: {
+        specs: 'spec/*Spec.js',
+        helpers: 'spec/*Helper.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'jasmine']);
 
 };
